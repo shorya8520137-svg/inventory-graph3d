ddcccfb46bf5ca14015196e761e3a173726e6a97
Shorya <shorya8520137@jmieti.edu.in>
2025-12-01 17:34:36 +0000

    Initial 3D graph project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div style={{ width: "100vw", height: "100vh" }}>
+            <Canvas camera={{ position: [5, 5, 10], fov: 50 }}>
+                <ambientLight intensity={0.6} />
+                <directionalLight position={[10, 10, 5]} intensity={1} />
+
+                {/* TEST 3D CUBE */}
+                <mesh>
+                    <boxGeometry args={[2, 2, 2]} />
+                    <meshStandardMaterial color={"hotpink"} />
+                </mesh>
+
+                <OrbitControls />
+            </Canvas>
+        </div>
+    );
 }
 
 export default App;
